6c7b132e76bd2cda2af0bbb42be427665fab1b8a
Nakulkomarla <nakulkolle@gmail.com>
2016-05-25 20:02:02 +0530

    Add files via upload

---
+++ assignment3.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package assignments;
+
+/**
+ * @author komarla
+ *
+ */
+public class assignment3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=5,y=10;
+		int a=x+y*2;
+		int b=x-y+2;
+		int c=(x+y)*2;
+		int d=(y%x);
+		System.out.println("a="+a+"b="+b+"c="+c+"d="+d);
+
+	}
+
+}
